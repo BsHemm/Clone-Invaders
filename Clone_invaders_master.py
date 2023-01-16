f68dbb52dc722440e03fc18d82bee0e352ebe795
Brian Lauber <blauber@arclia.com>
2023-01-15 20:25:34 -0500

    Added basic handling for a "Game Over" state

--- Clone_invaders_master.py
+++ Clone_invaders_master.py
@@ -25,54 +25,77 @@ displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Clone Invaders")
 
 
-
-# GUN_char instance
-gun_char=Gun_char()
-game_state.all_sprites.add(gun_char)
-#define Gun_char health? INstance?
-#enemy creation arrangemnt loopS
-for x in range(50,WIDTH-200,75):
-    for y in range(50,HEIGHT-375,75):
-
-        #enemies INstances
-        # enemy_a=Enemy_A(x*75,50)
-        enemy=Enemy_B(x,y)
-        game_state.all_sprites.add(enemy)
-        game_state.enemy_ships.add(enemy)
-
-#Lose conditions; conditions that make teh game over screen come up
-#press p (applied), endzone crossed ( not applied), player loses all health (not applied)
-#enemy crosses into endzone
-#gunchar health is <=0
+g=Game_Over(x=WIDTH/2,y=HEIGHT/2)
+game_state.game_over_sprites.add(g)
 
 
+def reset_game():
+    game_state.game_over = False
+    game_state.all_sprites.empty()
+    game_state.enemy_ships.empty()
 
+    # GUN_char instance
+    gun_char=Gun_char()
+    game_state.all_sprites.add(gun_char)
+    #define Gun_char health? INstance?
+    #enemy creation arrangemnt loopS
+    for x in range(50,WIDTH-200,75):
+        for y in range(50,HEIGHT-375,75):
+
+            #enemies INstances
+            # enemy_a=Enemy_A(x*75,50)
+            enemy=Enemy_B(x,y)
+            game_state.all_sprites.add(enemy)
+            game_state.enemy_ships.add(enemy)
+
+    #Lose conditions; conditions that make teh game over screen come up
+    #press p (applied), endzone crossed ( not applied), player loses all health (not applied)
+    #enemy crosses into endzone
+    #gunchar health is <=0
+
+
+reset_game()
         
 #game loop
 while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[K_p]:
-        g=Game_Over(x=WIDTH/2,y=HEIGHT/2)
-        game_state.all_sprites.add(g)
-        
+    while not game_state.game_over:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_p]:
+            game_state.game_over = True
 
-    game_state.all_sprites.update()
-    if armada.hit_wall:
-        armada.direction=-armada.direction
-        armada.reset()
-        for ship in game_state.enemy_ships:
-            ship.rect.y +=50 
-    #lose conditions
-    #if player_health <=0:
-        #player_health=-player_health
-        #im = Image.open("bear.png")
-        #im.show()
+        game_state.all_sprites.update()
+        if armada.hit_wall:
+            armada.direction=-armada.direction
+            armada.reset()
+            for ship in game_state.enemy_ships:
+                ship.rect.y +=50 
+        #lose conditions
+        #if player_health <=0:
+            #player_health=-player_health
+            #im = Image.open("bear.png")
+            #im.show()
 
-    displaysurface.fill((0,0,0))
-    game_state.all_sprites.draw(displaysurface)
-    pygame.display.update()
-    clock.tick(FPS)
+        displaysurface.fill((0,0,0))
+        game_state.all_sprites.draw(displaysurface)
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+    while game_state.game_over:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_RETURN]:
+            reset_game()
+
+        displaysurface.fill((0,0,0))
+        game_state.game_over_sprites.draw(displaysurface)
+        pygame.display.update()
+        clock.tick(FPS)
--- game_state.py
+++ game_state.py
@@ -3,4 +3,7 @@ import pygame
 
 #sprite Group
 all_sprites = pygame.sprite.Group()
-enemy_ships=pygame.sprite.Group() 
+enemy_ships=pygame.sprite.Group()
+game_over_sprites = pygame.sprite.Group()
+
+game_over = False
